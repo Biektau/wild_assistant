abe028e3667e9f555f71f1ddf26c0f78e90ed900
Ruzal <ruzal02@inbox.ru>
2024-02-17 18:58:31 +0300

    updated

--- routes/page_routes.js
+++ routes/page_routes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { get_home_page } from "../controllers/page_controllers.js";
+import {
+  get_home_page,
+  get_instagram_bonus_page,
+} from "../controllers/page_controllers.js";
 
 const route = express.Router();
 
 route.get("/", get_home_page);
+route.get("/instagram_bonus", get_instagram_bonus_page);
 
 export default route;
